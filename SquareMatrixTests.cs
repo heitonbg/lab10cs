d51eb78eea8af4ab78cdd5cac4ce29eee173259b
iwrawww@gmail.com <iwrawww@gmail.com>
2025-05-05 17:03:08 +0700

    скачал пакеты xunit

--- SquareMatrixTests.cs
+++ SquareMatrixTests.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xunit;
 
-namespace lab10cs
+public class SquareMatrixTests
 {
-    internal class SquareMatrixTests
-    {
-    }
+
 }
